ebd12d5ae9ad1b40e994bb3b31869245c66f23d7
jhammarstedt <52280124+jhammarstedt@users.noreply.github.com>
2021-04-19 18:07:48 +0200

    Update generate_output.py

--- data/generate_output.py
+++ data/generate_output.py
@@ -6,14 +6,14 @@ with open("output.json","r") as f:
 
 # First I just want to get the new data statistics as a markdown to the index file
 commit = data["commit_info"]["id"]
-commit = data["commit_info"]["time"]
+date = data["commit_info"]["time"]
 mean = data["benchmarks"][0]["stats"]["mean"]
 mini = data["benchmarks"][0]["stats"]["mean"]
 maxi = data["benchmarks"][0]["stats"]["max"]
 std = data["benchmarks"][0]["stats"]["stddev"]
 #print(data["benchmarks"]['stats'])
 output = markdown.markdown(f'''
-# This is the latest statistics from {time}
+# This is the latest statistics from {date}
 | Commit    | Mean  | Stddev|
 |----       |----   |----   |
 | {commit}  |{mean} |{std}  |
