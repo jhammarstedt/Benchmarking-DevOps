24c1f054cd49e77bd3473d4ac67e34bd64360d03
jhammarstedt <johan.hammarstedt@gmail.com>
2021-04-19 17:58:54 +0200

    added working output generation script

---
+++ data/generate_output.py
@@ -0,0 +1,24 @@
+import json
+import markdown
+
+with open("output.json","r") as f:
+    data = json.load(f)
+
+# First I just want to get the new data statistics as a markdown to the index file
+commit = data["commit_info"]["id"]
+commit = data["commit_info"]["time"]
+mean = data["benchmarks"][0]["stats"]["mean"]
+mini = data["benchmarks"][0]["stats"]["mean"]
+maxi = data["benchmarks"][0]["stats"]["max"]
+std = data["benchmarks"][0]["stats"]["stddev"]
+#print(data["benchmarks"]['stats'])
+output = markdown.markdown(f'''
+# This is the latest statistics from {time}
+| Commit    | Mean  | Stddev|
+|----       |----   |----   |
+| {commit}  |{mean} |{std}  |
+
+''')
+print(output)
+with open('docs/index.md','w') as f:
+    json.dump(output,f)
