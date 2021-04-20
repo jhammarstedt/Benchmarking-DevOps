41f7a32abc4d7abd584c0ede8047ad76bf62ace8
jhammarstedt <johan.hammarstedt@gmail.com>
2021-04-20 16:15:58 +0200

    added working table editor

--- data/generate_output.py
+++ data/generate_output.py
@@ -1,4 +1,6 @@
 import json
+from bs4 import BeautifulSoup
+import os
 
 with open("output.json","r") as f:
     data = json.load(f)
@@ -10,9 +12,24 @@ mean = data["benchmarks"][0]["stats"]["mean"]
 mini = data["benchmarks"][0]["stats"]["mean"]
 maxi = data["benchmarks"][0]["stats"]["max"]
 std = data["benchmarks"][0]["stats"]["stddev"]
-#print(data["benchmarks"]['stats'])
-output= f"# This is the latest statistics from {date}\n| Commit    | Mean  | Stddev|\n|----       |----   |----   |\n| {commit}  |{mean} |{std}  |"
-print(output)
-with open('docs/index.md','w') as f:
-    f.write(output)
-    f.close
+
+
+new_data = f"""<tr>
+                <td>{commit} </td>
+                <td>{date}</td>
+                <td>{mean}</td>
+                <td>{maxi}</td>
+                <td>{mini}</td>
+                <td>{std}</td>
+            </tr>"""
+
+
+html = open('.\docs\index.html')
+soup = BeautifulSoup(html,'html.parser')
+soup.tbody.tr.append(BeautifulSoup( new_data,'html.parser'))
+html.close()
+
+new_html = soup.prettify(soup.original_encoding)
+with open("docs/index.html",'w') as f:
+      f.write(new_html)
+      f.close()
--- docs/index.md
+++
@@ -1,4 +0,0 @@
-# This is the latest statistics from 2021-04-20T10:09:06+02:00
-| Commit    | Mean  | Stddev|
-|----       |----   |----   |
-| f75132f40de677b02220424a452ca05ad16b69f0  |0.00013044293986262364 |1.1440257859469113e-06  |
