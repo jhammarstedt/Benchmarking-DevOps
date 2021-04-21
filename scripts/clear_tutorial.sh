bbbbcc674d8bbb73793b5cf95d50bce59bb2ae87
jhammarstedt <johan.hammarstedt@gmail.com>
2021-04-21 14:47:53 +0200

    added script for tutorial

---
+++ scripts/clear_tutorial.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# ONLY FOR THE KATACODA TUTORIAL
+# Ignore otherwise
+
+# Cleaning out the repo for the tutorial
+echo "Removing files for tutorial"
+rm -rf .github src 
+rm output.json 
+echo "Necessary files are removed"
