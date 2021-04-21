bc5bf847656d0146a279116a2a158f1031fc86f7
jhammarstedt <johan.hammarstedt@gmail.com>
2021-04-21 11:24:09 +0200

    structure changes and added test2 for benchmarking

---
+++ src/benchmarking.py
@@ -0,0 +1,26 @@
+import pytest
+import time
+
+
+def test_case1(n):
+    """Regular for loop"""
+    x = [] 
+    for i in range(n,0,-1):
+        x.append(str(i))
+    
+    s= ""
+    for i in x:
+        s+= i
+
+
+def test_case2(n):
+    """Faster with list comprehenssion and join"""
+    x = [str(i) for i in range(n,0,-1)]
+    s = "".join(x)
+    
+
+def run_test(benchmark):
+    benchmark(test_case1(n=1000000))
+
+if __name__ =="__main__":
+    run_test()
