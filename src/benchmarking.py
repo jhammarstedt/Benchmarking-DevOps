94ac6a265a80f537daa1d747f959e80ab286ba5c
jhammarstedt <52280124+jhammarstedt@users.noreply.github.com>
2021-04-21 11:27:08 +0200

    Update benchmarking.py

--- src/benchmarking.py
+++ src/benchmarking.py
@@ -2,7 +2,7 @@ import pytest
 import time
 
 
-def test_case1(n):
+def test_case1(n=1000000):
     """Regular for loop"""
     x = [] 
     for i in range(n,0,-1):
@@ -13,14 +13,14 @@ def test_case1(n):
         s+= i
 
 
-def test_case2(n):
+def test_case2(n=1000000):
     """Faster with list comprehenssion and join"""
     x = [str(i) for i in range(n,0,-1)]
     s = "".join(x)
     
 
 def run_test(benchmark):
-    benchmark(test_case2(n=1000000))
+    benchmark(test_case2)
 
 if __name__ =="__main__":
     run_test()
