b5bb352930fd045e4ac9f57f5664fa2c944679a1
jhammarstedt <52280124+jhammarstedt@users.noreply.github.com>
2021-04-23 11:26:45 +0200

    Update clear_tutorial.sh

--- scripts/clear_tutorial.sh
+++ scripts/clear_tutorial.sh
@@ -6,4 +6,11 @@
 echo "Removing files for tutorial"
 rm -rf .github src 
 rm output.json 
+rm README.md
+
+cat <<EOM> README.md
+# ghActions tutorial in katacoda
+This is a katacoda tutorial template to test github actions.
+Created by @jhammarstedt and @carllei
+
 echo "Necessary files are removed"
