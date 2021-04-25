d7be882015a27ccd6cbafde37f266a43c78e1032
jhammarstedt <johan.hammarstedt@gmail.com>
2021-04-25 12:06:03 +0200

    added EA

--- scripts/collectEgg.sh
+++ scripts/collectEgg.sh
@@ -1,26 +1,28 @@
+#!/bin/bash
 echo "You find something strange hidden in your workflow file....."
-read -p "What item did you bring?": 'passvar
-echo
-if [$passvar = "shovel"]
-then
-    cat <<EOM> .github/workflows/tryme.yml
-    name: "Try me!"
-    on:
-      issues:
-        types: [opened, reopened]
+echo "What item did you bring?: "
+read item
+shift 2
 
-    jobs:
-      test:
-        name: setup environment
-        runs-on: ubuntu-latest
-        steps:
-          - name: Improving issues
-            uses: deep5050/memes-on-issues-action@main
-            with:
-              GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
-    EOM
-    echo " A WILD WORKFLOW APPEARS"
-    echo " Do you have any ISSUES with that?"
+if [[ "$item" == "shovel" ]]; then
+cat <<EOM>> .github/workflows/tryme.yml
+name: "Try me!"
+on:
+  issues:
+  types: [opened, reopened]
+
+  jobs:
+   test:
+     name: setup environment
+     runs-on: ubuntu-latest
+     steps:
+      - name: Improving issues
+        uses: deep5050/memes-on-issues-action@main
+        with:
+	    GITHUB_TOKEN: \${{secrets.GITHUB_TOKEN}}
+EOM
+echo "A WILD WORKFLOW APPEARS"
+echo "Do you have any ISSUES with that?"
 else
-    echo "Wrong password, did you even read the first instructions?"
+	echo "Wrong password, did you even read the first instructions?"
 fi
